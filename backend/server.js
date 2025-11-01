a40699fc152238125dcd8e3b7e471a1bf3638bd8
TMG-WE <sivasubbumobiles@gmail.com>
2025-11-01 14:14:58 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://tmg-mass-gaja-ai-chatbot-z4hp.vercel.app"
+    "https://tmg-mass-gaja-ai-chatbot-ah2y.vercel.app/"
   ]
 }));
 app.use(express.json());
